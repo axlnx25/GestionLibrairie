c7772b8f6e1fe37b77b8c08bd217aed1d99c232e
OUEDRAOGO Abdun Nourr <ouedraogoabdunnourr@gmail.com>
2025-11-02 21:52:59 +0000

    Change database password to placeholder
    Updated database password to a placeholder value.

--- src/app_dao/ConnectionDataBase.java
+++ src/app_dao/ConnectionDataBase.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class ConnectionDataBase {
     private static final String URL = "jdbc:mysql://localhost:3306/GestionLibrairie?serverTimezone=UTC";
     private static final String USER = "root";
-    private static final String PASSWORD ="ouedraogo";
+    private static final String PASSWORD ="xxxxxx";
 
     private static Connection connection;
 
